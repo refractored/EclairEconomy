fe78ef4b77788a1bec68bb64e4aefb6cf276f196
refractored <me@refractored.net>
2026-08-10 11:46:50 -0500

    feat(config): Setup config.yml loading
    Signed-off-by: refractored <me@refractored.net>

--- src/main/kotlin/net/refractored/eclairEconomy/EclairEconomy.kt
+++ src/main/kotlin/net/refractored/eclairEconomy/EclairEconomy.kt
@@ -1,13 +1,58 @@
 package net.refractored.eclairEconomy
 
 import com.github.shynixn.mccoroutine.folia.SuspendingJavaPlugin
+import org.spongepowered.configurate.CommentedConfigurationNode
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader
+import java.io.File
+import java.nio.file.Files
 
 class EclairEconomy : SuspendingJavaPlugin() {
+    init {
+        instance = this
+    }
+
     override fun onEnable() {
-        // Plugin startup logic
+        reload()
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    fun reload() {
+        dataFolder.mkdirs()
+
+        loadConfig("config.yml")
+    }
+
+    private fun loadConfig(file: String): CommentedConfigurationNode {
+        if (!File(dataFolder, file).exists()) {
+            javaClass.getResourceAsStream("/$file")?.let {
+                Files.copy(
+                    it,
+                    dataFolder.toPath().resolve(file),
+                )
+            }
+        }
+
+        val loader =
+            YamlConfigurationLoader
+                .builder()
+                .defaultOptions { options ->
+                    options.serializers { builder -> // FUTURE USE
+                    }
+                }.path(dataFolder.toPath().resolve(file))
+                .build()
+
+        return loader.load()
+    }
+
+    companion object {
+        /**
+         * The plugin's instance
+         */
+        @JvmStatic
+        lateinit var instance: EclairEconomy
+            private set
     }
 }
