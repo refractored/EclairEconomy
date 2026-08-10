25576d047c134c21e5cc5f2e9411319b605175db
refractored <me@refractored.net>
2026-08-10 11:46:02 -0500

    feat(config): add configurate as a lib
    Signed-off-by: refractored <me@refractored.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
+import org.gradle.kotlin.dsl.buildConfigField
+
 plugins {
+    java
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.plugin.yaml.creator.paper)
     alias(libs.plugins.gversion)
     alias(libs.plugins.buildconfig)
     alias(libs.plugins.paper.run)
 }
+
+group = "net.refractored"
+version = "0.1"
+val targetJavaVersion = 25
+
+val externalDepends = mutableListOf<String>()
 
 repositories {
     mavenCentral()
@@ -13,16 +22,19 @@ repositories {
 
 dependencies {
     compileOnly(libs.paper.api)
+
     implementation(libs.kotlin)
     implementation(libs.kotlin.coroutines)
     implementation(libs.bundles.mccoroutine)
+    implementation(libs.bundles.configurate)
 }
 
 paperPluginYaml {
-    main = "net.refractored.eclairEconomy.EclairEconomy"
-    loader = "net.refractored.eclairEconomy.EclairEconomyLoader"
+    main = "$group.eclairEconomy.EclairEconomy"
+    loader = "$group.eclairEconomy.EclairEconomyLoader"
     foliaSupported = true // Probably would need more testing.
     apiVersion = "26.2"
+    version = "$version"
 
     authors = listOf("refractored")
     contributors = listOf()
@@ -30,8 +42,23 @@ paperPluginYaml {
     website = "https://github.com/refractored/EclairEconomy"
 }
 
+gversion {
+    srcDir = "src/main/kotlin"
+    classPackage = "$group.eclairEconomy"
+    className = "BuildConstants"
+    language = "kotlin"
+    explicitType = true
+}
+
+buildConfig {
+    className("Libraries")
+    useJavaOutput()
+
+    buildConfigField("DEPENDS", externalDepends)
+}
+
 kotlin {
-    jvmToolchain(25)
+    jvmToolchain(targetJavaVersion)
 }
 
 tasks {
