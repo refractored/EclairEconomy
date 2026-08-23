8f2f63c1556cdf2c691bd82309b5986bcd1753c5
refractored <me@refractored.net>
2026-08-23 11:50:31 -0500

    feat(api): include BuildConstants and update gradle

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,6 +3,21 @@ import org.gradle.kotlin.dsl.kotlin
 
 plugins {
     `java-library`
+    alias(libs.plugins.gversion)
 }
 
 val targetJavaVersion = 25
+
+gversion {
+    srcDir = "src/main/java"
+    classPackage = "$group.eclairEconomy.api"
+    className = "BuildConstants"
+    language = "java"
+    explicitType = true
+}
+
+tasks {
+    build {
+        dependsOn(createVersionFile)
+    }
+}
